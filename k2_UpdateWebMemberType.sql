3214c3a45518629d9960b87b8b1edf839e95b1f6
mapleleaflandshark <45762870+mapleleaflandshark@users.noreply.github.com>
2019-06-24 09:35:31 -0400

    Create k2_UpdateWebMemberType.sql

---
+++ k2_UpdateWebMemberType.sql
@@ -0,0 +1,38 @@
+alter proc k2_UpdateWebMemberType(@triggerid varchar(10))
+as
+begin
+
+	insert into Name_Log (DATE_TIME,LOG_TYPE,SUB_TYPE,USER_ID,ID,LOG_TEXT)
+	select	getDate(),
+				'CHANGE',
+				'CHANGE',
+				UPDATED_BY,
+				ID,
+				'Name.MIDDLE_NAME: ' + MIDDLE_NAME + ' -> '
+	from		Name
+	where		ID = @triggerid
+	and		MEMBER_TYPE = 'NM'
+	and		MIDDLE_NAME = 'PROS'
+
+	insert into Name_Log (DATE_TIME,LOG_TYPE,SUB_TYPE,USER_ID,ID,LOG_TEXT)
+	select	getDate(),
+				'CHANGE',
+				'CHANGE',
+				UPDATED_BY,
+				ID,
+				'Name.MEMBER_TYPE: ' + MEMBER_TYPE + ' -> ' + MIDDLE_NAME
+	from		Name
+	where		ID = @triggerid
+	and		MEMBER_TYPE = 'NM'
+	and		MIDDLE_NAME = 'PROS'
+
+	update	Name
+	set		MEMBER_TYPE = MIDDLE_NAME,
+				MIDDLE_NAME = '',,
+				FULL_NAME = FIRST_NAME + ' ' + LAST_NAME,
+				LAST_UPDATED = getDate()
+	where		ID = @triggerid
+	and		MEMBER_TYPE = 'NM'
+	and		MIDDLE_NAME = 'PROS'
+
+end

79896c8b247a0dab6ea6393f38b959d50531b48f
mapleleaflandshark <45762870+mapleleaflandshark@users.noreply.github.com>
2019-06-24 09:36:10 -0400

    Update k2_UpdateWebMemberType.sql

--- k2_UpdateWebMemberType.sql
+++ k2_UpdateWebMemberType.sql
@@ -4,35 +4,35 @@ begin
 
 	insert into Name_Log (DATE_TIME,LOG_TYPE,SUB_TYPE,USER_ID,ID,LOG_TEXT)
 	select	getDate(),
-				'CHANGE',
-				'CHANGE',
-				UPDATED_BY,
-				ID,
-				'Name.MIDDLE_NAME: ' + MIDDLE_NAME + ' -> '
-	from		Name
-	where		ID = @triggerid
-	and		MEMBER_TYPE = 'NM'
-	and		MIDDLE_NAME = 'PROS'
+		'CHANGE',
+		'CHANGE',
+		UPDATED_BY,
+		ID,
+		'Name.MIDDLE_NAME: ' + MIDDLE_NAME + ' -> '
+	from	Name
+	where	ID = @triggerid
+	and	MEMBER_TYPE = 'NM'
+	and	MIDDLE_NAME = 'PROS'
 
 	insert into Name_Log (DATE_TIME,LOG_TYPE,SUB_TYPE,USER_ID,ID,LOG_TEXT)
 	select	getDate(),
-				'CHANGE',
-				'CHANGE',
-				UPDATED_BY,
-				ID,
-				'Name.MEMBER_TYPE: ' + MEMBER_TYPE + ' -> ' + MIDDLE_NAME
-	from		Name
-	where		ID = @triggerid
-	and		MEMBER_TYPE = 'NM'
-	and		MIDDLE_NAME = 'PROS'
+		'CHANGE',
+		'CHANGE',
+		UPDATED_BY,
+		ID,
+		'Name.MEMBER_TYPE: ' + MEMBER_TYPE + ' -> ' + MIDDLE_NAME
+	from	Name
+	where	ID = @triggerid
+	and	MEMBER_TYPE = 'NM'
+	and	MIDDLE_NAME = 'PROS'
 
 	update	Name
-	set		MEMBER_TYPE = MIDDLE_NAME,
-				MIDDLE_NAME = '',,
-				FULL_NAME = FIRST_NAME + ' ' + LAST_NAME,
-				LAST_UPDATED = getDate()
-	where		ID = @triggerid
-	and		MEMBER_TYPE = 'NM'
-	and		MIDDLE_NAME = 'PROS'
+	set	MEMBER_TYPE = MIDDLE_NAME,
+		MIDDLE_NAME = '',
+		FULL_NAME = FIRST_NAME + ' ' + LAST_NAME,
+		LAST_UPDATED = getDate()
+	where	ID = @triggerid
+	and	MEMBER_TYPE = 'NM'
+	and	MIDDLE_NAME = 'PROS'
 
 end
